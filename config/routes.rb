8149ff795370126263b1e62ab2049e3a2b331d23
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-02-03 07:42:49 +0000

    3 Bootstrap FontAwesome導入

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  
+  root to: "public/homes#top"
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
