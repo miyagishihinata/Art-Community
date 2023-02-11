98038ed9758825ab38fcb9cbcb70ab32741d788d
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-02-11 06:57:53 +0000

    28 user.rb フォロー機能追記 relationshipsコントローラ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,19 @@ class User < ApplicationRecord
   # フォロー一覧画面,フォロワー一覧画面で使う
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
 
 
 
