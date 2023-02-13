60b7587539f60a9b93f0acf36c2af4e9cf3f5824
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-02-13 09:28:27 +0000

    36 タイムライン画面、コントローラー 編集

--- app/controllers/public/timelines_controller.rb
+++ app/controllers/public/timelines_controller.rb
@@ -1,2 +1,6 @@
 class Public::TimelinesController < ApplicationController
+  def index
+    @illustrations = Illustration.where(user_id: [current_user.id, *current_user.following_ids])
+    @users = User.where(user_id: [current_user.id, *current_user.following_ids])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,9 @@ Rails.application.routes.draw do
       resources :comments, only: [:create, :destroy] #コメント機能
     end
 
+    #タイムライン
+    get 'timelines' => 'timelines#index'
+
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
