0e7d129a9389bc6e26b29b78c823b90d1148af9b
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-02-13 06:05:39 +0000

    35 フォロー一覧、フォロワー一覧 編集

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -14,13 +14,13 @@ class Public::RelationshipsController < ApplicationController
   # フォロー一覧
   def followings
     user = User.find(params[:user_id])
-    @users = user.followings
+    @users = user.followings.page(params[:page])
   end
 
   # フォロワー一覧
   def followers
     user = User.find(params[:user_id])
-    @users = user.followers
+    @users = user.followers.page(params[:page])
   end
 
 end
