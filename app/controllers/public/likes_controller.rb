54395fc61771a6b1f8f62234d88e695990dae516
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-02-20 08:56:15 +0000

    44 通知機能

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,19 +1,5 @@
 class Public::LikesController < ApplicationController
   def create
-    illustration = Illustration.find(params[:illustration_id])
-    like = current_user.likes.new(illustration_id: illustration.id)
-    like.save
-    redirect_to illustration_path(illustration.id)
-  end
-
-  def destroy
-    illustration = Illustration.find(params[:illustration_id])
-    like = current_user.likes.find_by(illustration_id: illustration.id)
-    like.destroy
-    redirect_to illustration_path(illustration.id)
-  end
-
-  def create
     like = current_user.likes.new(illustration_id: @illustration.id)
     like.save
     @illustration = Illustration.find(params[:illustration_id])
@@ -24,5 +10,13 @@ class Public::LikesController < ApplicationController
       format.js
     end
   end
+
+  def destroy
+    illustration = Illustration.find(params[:illustration_id])
+    like = current_user.likes.find_by(illustration_id: illustration.id)
+    like.destroy
+    redirect_to illustration_path(illustration.id)
+  end
+
 
 end
