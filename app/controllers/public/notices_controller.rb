cc6661aead67bcf08dda6bb44fe612b016dbfbb2
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-03-02 12:40:36 +0000

    63 イラスト詳細画面　コメント部分修正

--- app/controllers/public/notices_controller.rb
+++ app/controllers/public/notices_controller.rb
@@ -1,6 +1,8 @@
 class Public::NoticesController < ApplicationController
   def index
     @notices = current_user.passive_notices.page(params[:page]).per(20)
+    #自分の投稿に対するいいね、コメントは通知に表示しない↓
+    @notices = @notices.where.not(visitor_id: current_user.id)
     @notices.where(checked: false).each do |notice|
       notice.update!(checked: true)
     end
