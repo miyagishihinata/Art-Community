debc6e01919d7f434023ce40523951432f7b3a64
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2023-03-08 10:51:22 +0000

    78 コメント送信時　フラッシュメッセージ調整

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,20 +2,23 @@ class Public::CommentsController < ApplicationController
  before_action :is_guest_user, only: [:create, :destroy]
 
   def create
-    illustration = Illustration.find(params[:illustration_id])
+    @illustration = Illustration.find(params[:illustration_id])
     post_comment = current_user.comments.new(comment_params)
-    post_comment.illustration_id = illustration.id
+    post_comment.illustration_id = @illustration.id
+   
 
-    if illustration = Illustration.find(params[:illustration_id])
-      post_comment.save
-      illustration.create_notice_comment!(current_user, post_comment.id)
-      flash[:notice] = "コメントの投稿に成功しました"
+    if  post_comment.save
+      @illustration.create_notice_comment!(current_user, post_comment.id)
+      flash[:notice] = "コメントの投稿に成功しました。"
       redirect_to illustration_path(params[:illustration_id])
 
     else
-      flash[:notice] = "コメントの投稿に失敗しました"
-      render 'illustrations/show'
+      @comment = Comment.new
+      @comments = @illustration.comments.where(parent_id: nil).order(created_at: :desc)
+      flash[:notice] = "コメントの投稿に失敗しました。"
+      render "public/illustrations/show"
     end
+
   end
 
 
